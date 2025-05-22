0ec62903bfb0d7f5a8fc1d5720a8aba3ad50f657
Kylin_on_Mac <kylingithubdev@gmail.com>
2025-05-22 10:17:40 +0800

    feat:update docker file and script

--- scripts/buildx_and_push.bash
+++ scripts/buildx_and_push.bash
@@ -1,6 +1,7 @@
 export tag="4.1.2"
 export game="maze_car"
+export pge_ver="PGE20250522"
 
 docker buildx build --platform linux/amd64,linux/arm64 \
--t paiatech/${game}:${tag} -t paiatech/${game}:${tag}-PGE20250313 \
+-t paiatech/${game}:${tag} -t paiatech/${game}:${tag}-${pge_ver} \
 -f ./Dockerfile . --push
